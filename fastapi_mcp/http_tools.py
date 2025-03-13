149d81b33b753ffa42a60ff80c853e8725fd8413
Natahlie Wang <natahlie.wang@shopify.com>
2025-03-13 17:18:33 -0400

    Restoring list import

--- fastapi_mcp/http_tools.py
+++ fastapi_mcp/http_tools.py
@@ -6,7 +6,7 @@ This module provides functionality for creating MCP tools from FastAPI endpoints
 
 import json
 import logging
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import httpx
 from fastapi import FastAPI
