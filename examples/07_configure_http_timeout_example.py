c891fe4c4036cefd349dd7ccd74b4a0ffeadc3a1
shiraayal-tadata <shira@tadata.com>
2025-04-17 16:45:37 +0300

    added http custom timeout example

---
+++ examples/07_configure_http_timeout_example.py
@@ -0,0 +1,24 @@
+"""
+This example shows how to configure the HTTP client timeout for the MCP server.
+In case you have API endpoints that take longer than 5 seconds to respond, you can increase the timeout.
+"""
+import httpx
+
+from examples.shared.apps.items import app
+from examples.shared.setup import setup_logging
+
+from fastapi_mcp import FastApiMCP
+
+setup_logging()
+
+mcp = FastApiMCP(
+    app,
+    http_client=httpx.AsyncClient(timeout=10)
+)
+mcp.mount(mcp)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
